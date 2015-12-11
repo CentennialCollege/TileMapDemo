0e99bcd415c20db6beecd3f77ae298f70cc92754
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-12-11 18:32:13 -0500

    created basic tile map structure

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,1 +1,68 @@
+/// <reference path="_reference.ts" />
+// Global Game Variables
+var assets;
+var manifest = [
+    { id: "tile", src: "../../Assets/images/tile.png" },
+    { id: "utile", src: "../../Assets/images/utile.png" },
+    { id: "ptile", src: "../../Assets/images/ptile.png" }
+];
+var gameLevelMap = ["T", "T", "T", "P", "P", "P", "T", "T",
+    "P", "P", "T", "P", "T", "P", "P", "T",
+    "T", "P", "T", "P", "T", "T", "P", "T",
+    "T", "P", "T", "P", "T", "T", "P", "T",
+    "T", "P", "P", "P", "T", "T", "P", "P",
+    "U", "U", "U", "U", "U", "U", "U", "U",
+];
+var tileMap = new Array(); // new bitmap array
+var canvas;
+var stage;
+var game;
+var tile;
+function preload() {
+    assets = new createjs.LoadQueue();
+    assets.installPlugin(createjs.Sound);
+    assets.on("complete", init, this);
+    assets.loadManifest(manifest);
+}
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    game = new createjs.Container();
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.on("tick", gameLoop);
+    main();
+}
+function gameLoop(event) {
+    stage.update();
+}
+function createTileMap() {
+    var index = 0;
+    for (var row = 0; row < 6; row++) {
+        for (var col = 0; col < 8; col++) {
+            var tempTile;
+            switch (gameLevelMap[index]) {
+                case "T":
+                    tempTile = new createjs.Bitmap(assets.getResult("tile"));
+                    break;
+                case "U":
+                    tempTile = new createjs.Bitmap(assets.getResult("utile"));
+                    break;
+                case "P":
+                    tempTile = new createjs.Bitmap(assets.getResult("ptile"));
+                    break;
+            }
+            var tileY = row * 80;
+            var tileX = col * 80;
+            tileMap.push(tempTile);
+            tempTile.x = tileX;
+            tempTile.y = tileY;
+            game.addChild(tempTile);
+            index++;
+        }
+    }
+}
+function main() {
+    createTileMap();
+    stage.addChild(game);
+}
 //# sourceMappingURL=game.js.map
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,14 +1,28 @@
 /// <reference path="_reference.ts" />
 
 // Global Game Variables
-var assets:createjs.LoadQueue;
-var manifest = [{id: "tile", src:"Assets/images/tile.png"}];
+var assets: createjs.LoadQueue;
+var manifest = [
+				{ id: "tile", src: "../../Assets/images/tile.png" },
+				{ id: "utile", src: "../../Assets/images/utile.png" },
+				{ id: "ptile", src: "../../Assets/images/ptile.png" }
+];
 
-var canvas:HTMLElement;
-var stage:createjs.Stage;
-var game:createjs.Container;
+var gameLevelMap: string[] = [	"T", "T", "T", "P", "P", "P", "T", "T",
+								"P", "P", "T", "P", "T", "P", "P", "T",
+								"T", "P", "T", "P", "T", "T", "P", "T",
+								"T", "P", "T", "P", "T", "T", "P", "T",
+								"T", "P", "P", "P", "T", "T", "P", "P",
+								"U", "U", "U", "U", "U", "U", "U", "U",
+];
 
-var tile:createjs.Bitmap;
+var tileMap: createjs.Bitmap[] = new Array<createjs.Bitmap>(); // new bitmap array
+
+var canvas: HTMLElement;
+var stage: createjs.Stage;
+var game: createjs.Container;
+
+var tile: createjs.Bitmap;
 
 function preload() {
 	assets = new createjs.LoadQueue();
@@ -26,13 +40,41 @@ function init() {
 	main();
 }
 
-function gameLoop(event:createjs.Event) {
+function gameLoop(event: createjs.Event) {
 	stage.update();
 }
 
+function createTileMap() {
+	var index = 0;
+	for (var row = 0; row < 6; row++) {
+		for (var col = 0; col < 8; col++) {
+			var tempTile: createjs.Bitmap;
+			switch(gameLevelMap[index]) {
+				case "T":
+					tempTile = new createjs.Bitmap(assets.getResult("tile"));
+				break;
+				case "U":
+					tempTile = new createjs.Bitmap(assets.getResult("utile"));
+				break;
+				case "P":
+					tempTile = new createjs.Bitmap(assets.getResult("ptile"));
+				break;
+			}
+			
+			var tileY = row * 80;
+			var tileX = col * 80;
+			tileMap.push(tempTile);
+			tempTile.x = tileX;
+			tempTile.y = tileY;
+			game.addChild(tempTile);
+			index++;
+		}
+	}
+}
+
 function main() {
-	tile = new createjs.Bitmap(assets.getResult("tile"));
-	game.addChild(tile);
-	
+	createTileMap();
+
+
 	stage.addChild(game);
 }
